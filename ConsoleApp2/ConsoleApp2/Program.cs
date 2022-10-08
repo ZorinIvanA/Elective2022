ee56a64b7dedb2106f136c18a24556bed3e7f88b
ИВАН ЗОРИН <zorin.ia@misis.ru>
2022-10-08 12:50:53 +0300

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа через запятую");
+            var input = Console.ReadLine();
+            var numbers = input.Split(',');
+
+            foreach (var s in numbers)
+            {
+                int result = 0;
+                if (!int.TryParse(s, out result))
+                {
+                    Console.WriteLine("Ошибка, введите целые числа");
+                }
+
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
