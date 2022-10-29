4de29ff84540adf34d7220ea8aad9277e8e001e7
zorinivana <zorin.ivan@outlook.com>
2022-10-29 12:37:18 +0300

    Рефакторинг

--- ConsoleApp2/Books/BooksRepository.cs
+++ ConsoleApp2/Books/BooksRepository.cs
@@ -2,6 +2,7 @@
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Books
 {
@@ -14,38 +15,29 @@ namespace Books
 
         public List<Book> GetAll()
         {
-            var connectionString = GetConnectionString();
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var books = new List<Book>();
-                connection.Open();
-                using (var sqlCommand = new SqlCommand("SELECT id, Name FROM Books", connection))
-                {
-                    var reader = sqlCommand.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        books.Add(new Book
-                        {
-                            Id = int.Parse(reader["Id"].ToString()),
-                            Name = reader["Name"].ToString(),
-                        });
-                    }
-                }
-                connection.Close();
-                return books;
-            }
+            return this.ExecuteCommandWithQuery($"SELECT id, Name FROM Books WHERE id={id}");
         }
 
         public Book GetById(int id)
         {
+            var books = this.ExecuteCommandWithQuery($"SELECT id, Name FROM Books WHERE id={id}");
+
+            return books.FirstOrDefault();
+        }
+
+        private List<Book> ExecuteCommandWithQuery(string command)
+        {
             var connectionString = GetConnectionString();
+
             using (var connection = new SqlConnection(connectionString))
             {
-                var books = new List<Book>();
                 connection.Open();
+                var commandText = command;
+                List<Book> books = new List<Book>();
 
-                using (var sqlCommand = new SqlCommand($"SELECT id, Name FROM Books WHERE id={id}", connection))
+                using (var sqlCommand = new SqlCommand(commandText, connection))
                 {
+
                     var reader = sqlCommand.ExecuteReader();
                     while (reader.Read())
                     {
@@ -56,60 +48,35 @@ namespace Books
                         });
                     }
                 }
-
                 connection.Close();
 
-                return books.FirstOrDefault();
+                return books;
             }
         }
 
         public void Insert(Book book)
         {
-            var connectionString = GetConnectionString();
-
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                var commandText =
-                    $"INSERT INTO Books(Name, Author_id) VALUES ('{book.Name}', {book.AuthorId})";
-
-                using (var sqlCommand = new SqlCommand(commandText, connection))
-                {
-                    var reader = sqlCommand.ExecuteNonQuery();
-                }
-
-                connection.Close();                
-            }
+            this.ExecuteCommand($"INSERT INTO Books(Name, Author_id) VALUES ('{book.Name}', {book.AuthorId})");
         }
 
         public void Update(Book book)
         {
-            var connectionString = GetConnectionString();
-            using (var connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                var commandText =
-                    $"UPDATE Books SET Name = '{book.Name}', Author_Id= {book.AuthorId} WHERE Id={book.Id}";
-
-                using (var sqlCommand = new SqlCommand(commandText, connection))
-                {
-                    var reader = sqlCommand.ExecuteNonQuery();
-                }
-
-                connection.Close();
-            }
+            this.ExecuteCommand($"UPDATE Books SET Name = '{book.Name}', Author_Id= {book.AuthorId} WHERE Id={book.Id}");
         }
 
         public void Delete(int id)
+        {
+            this.ExecuteCommand($"DELETE FROM Books WHERE Id={id}");
+        }
+
+        private void ExecuteCommand(string command)
         {
             var connectionString = GetConnectionString();
 
             using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                var commandText =
-                    $"DELETE FROM Books WHERE Id={id}";
+                var commandText = command;
 
                 using (var sqlCommand = new SqlCommand(commandText, connection))
                 {
